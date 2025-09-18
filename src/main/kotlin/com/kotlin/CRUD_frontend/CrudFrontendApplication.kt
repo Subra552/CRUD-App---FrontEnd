6d8548722f44bb32f16ee815e3bc1987b9daf1a1
Subra Verma <vsubra786@gmail.com>
2025-09-18 18:40:22 +0530

    first commit

---
+++ src/main/kotlin/com/kotlin/CRUD_frontend/CrudFrontendApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.CRUD_frontend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudFrontendApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudFrontendApplication>(*args)
+}
